e05ab688befff3ba8497175b18a26cda52c8ab29
mrApptastic <henrikbeske@gmail.com>
2020-11-09 12:20:27 +0100

    Added mrMr2 package to the project and added test path to tsconfig.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from "./nav-menu/nav-menu.component";
 import { HomeComponent } from "./home/home.component";
 import { CounterComponent } from "./counter/counter.component";
 import { FetchDataComponent } from "./fetch-data/fetch-data.component";
-import { ChatService } from "./services/chat-service";
+import { ChatService } from "@services/chat-service";
 import { ApiAuthorizationModule } from "src/api-authorization/api-authorization.module";
 import { AuthorizeGuard } from "src/api-authorization/authorize.guard";
 import { AuthorizeInterceptor } from "src/api-authorization/authorize.interceptor";
